2925156f78751d709e5ebf45ca210ee24adcb93f
Valerian Backeberg <valerian@backebergs.de>
2021-10-23 19:26:48 +0200

    Define Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
+import Assignments from "./Assignments/Assignments";
+import Boids from "./Boids/Boids";
 import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <main></main>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/boids">
+            <Boids />
+          </Route>
+          <Route path="/">
+            <Assignments />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
