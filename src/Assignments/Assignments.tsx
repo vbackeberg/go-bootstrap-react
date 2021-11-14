f4dbccd0d8925483702d4af92ebdf5dc0c6b73e5
Valerian Backeberg <valerian@backebergs.de>
2021-11-14 11:26:22 +0100

    Base state of the app

--- src/Assignments/Assignments.tsx
+++ src/Assignments/Assignments.tsx
@@ -13,55 +13,5 @@ export default function Assignments(): ReactElement {
     });
   }, []);
 
-  return (
-    <div className="container">
-      <h1 className="mb-5">Assignments</h1>
-      <div className="row gy-5 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
-        {assignments.map((assignment) => (
-          <div>
-            <div className="card">
-              <img src={exampleLogo} className="img-fluid" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">{assignment.name}</h5>
-                <p className="card-text">{assignment.description}</p>
-                <div className="d-flex justify-content-between">
-                  <button className="btn btn-outline-danger">Delete</button>
-                  <button className="btn btn-outline-primary">Edit</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-        {loading &&
-          [1, 2, 3, 4].map((_) => (
-            <div>
-              <div className="card">
-                <div
-                  style={{ height: "180px" }}
-                  className="bg-dark bg-opacity-25"
-                ></div>
-                <div className="card-body">
-                  <h5 className="card-title placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </h5>
-                  <div className="bg-dark bg-opacity-25"></div>
-                  <p className="card-text placeholder-glow">
-                    <span className="placeholder col-7"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-6"></span>
-                    <span className="placeholder col-8"></span>
-                  </p>
-
-                  <div className="d-flex justify-content-between">
-                    <button className="btn btn-outline-danger disabled placeholder col-3"></button>
-                    <button className="btn btn-outline-primary disabled placeholder col-2"></button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -3,38 +3,5 @@ import { Link } from "react-router-dom";
 import brand from "../assets/brand.svg";
 
 export default function Navbar(): ReactElement {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <img src={brand} height="38" width="38" alt="" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTop"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTop">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Assignments
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/boids">
-                Boids
-              </Link>
-            </li>
-          </ul>
-          <button className="btn btn-primary">
-            <i className="bi bi-person-fill"></i>
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
+  return <nav></nav>;
 }
